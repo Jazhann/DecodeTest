21d51016ea134e14f9cba085c8ca19fd19f62f16
Daniel Rodriguez <danirg@gmail.com>
2022-03-22 23:20:13 +0100

    Add sort and csv file save

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const fs = require('fs');
 
-const filePath = './assets/leaderboard.csv';
+const csvToReadPath = './assets/leaderboard.csv';
+const csvToWritePath = './leaderboardFixed.csv';
 
-fs.readFile(filePath, 'utf8' , (err, data) => {
+fs.readFile(csvToReadPath, 'utf8' , (err, data) => {
   if (err) {
     console.error('Error reading leaderboard file');
     return
@@ -11,21 +12,29 @@ fs.readFile(filePath, 'utf8' , (err, data) => {
 });
 
 function decode(data) {
-    const fileData = data.toString()
-        .replaceAll('\n', '')
-        .split("\r")
-        .map(line => line.split(','));
-    const result = fileData.map(line => ([line[0], getResult(line[1], line[2])]));
-    console.table(result);
-}
-
-function getResult(base, data) {
+  const fileData = data.toString()
+  .replaceAll('\n', '')
+  .split("\r")
+  .map(line => line.split(','));
+  const result = fileData.map(line => ([line[0], getResult(line[1], line[2])])).sort((a, b) => b[1] - a[1]);
+  saveCsv(result);
+  }
+  
+  function getResult(base, data) {
     let values = base.split('');
     values = values.reduce( (acc, value, index) => {
-        acc[value] = index;
-        return acc;
+      acc[value] = index;
+      return acc;
     }, {});
     let dataToDecimal = data.split('');
     dataToDecimal = dataToDecimal.map( data => values[data]);
     return parseInt(dataToDecimal.join(''), Object.keys(values).length);
+}
+
+function saveCsv(data) {
+  fs.writeFile(csvToWritePath, data.join('\n'), 'utf8', function (err) {
+    if (err) {
+      console.error('Error saving file');
+    }
+  });
 }
